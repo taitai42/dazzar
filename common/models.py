3dc6ae2ded56ae6cfa401995dc587457ccee5e7a
Vincent Philaeux Lamotte <vinc.lamotte@gmail.com>
2016-10-12 18:22:03 +0200

    Add right to model

--- common/models.py
+++ common/models.py
@@ -1,13 +1,24 @@
+from enum import IntEnum
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class UserBasicRights(IntEnum):
+    Default = 0
+    Banned = 1
+    AdminRights = 2
+    PlayVIP = 4
+    VouchVIP = 8
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.String(40), primary_key=True)
-    nickname = db.Column(db.String(80), nullable=True, index=True)
+    nickname = db.Column(db.String(20), nullable=True, index=True)
+    basic_rights = db.Column(db.Integer, nullable=False, default=0)
 
     def is_authenticated(self):
         return True
@@ -20,6 +31,12 @@ class User(db.Model):
 
     def get_id(self):
         return self.id
+
+    def has_right(self, right):
+        return (right | self.basic_rights) > 0
+
+    def give_rights(self, right):
+        self.basic_rights = self.basic_rights & right
 
     @staticmethod
     def get_or_create(steam_id):
