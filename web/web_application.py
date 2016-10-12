31ac8e1b38450929c34641e848279f692f5010ae
Vincent Philaeux Lamotte <vinc.lamotte@gmail.com>
2016-10-12 18:09:12 +0200

    Basic page architecture

--- web/web_application.py
+++ web/web_application.py
@@ -20,11 +20,11 @@ migrate = Migrate(app, db)
 oid = OpenID(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    logging.log(logging.WARNING, 'azda ' + user_id)
     user = User.query.filter_by(id=user_id).first()
     return user
 
@@ -32,22 +32,32 @@ def load_user(user_id):
 # Routes
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 
-@app.route('/hidden', methods=['GET'])
+@app.route('/ladder/play')
 @login_required
-def hidden():
-    return 'Hello World2'
+def ladder_play():
+    return render_template('ladder_play.html')
+
+
+@app.route('/ladder/scoreboard')
+def ladder_scoreboard():
+    return render_template('ladder_scoreboard.html')
 
 
 @app.route('/login')
-@oid.loginhandler
 def login():
+    return render_template('login.html')
+
+
+@app.route('/login/steam')
+@oid.loginhandler
+def login_steam():
     if current_user.is_authenticated:
-        return redirect(url_for('hidden'))
+        return redirect(url_for('index'))
     return oid.try_login('http://steamcommunity.com/openid')
 
 
@@ -59,7 +69,7 @@ def create_or_login(resp):
     match = _steam_id_re.search(resp.identity_url)
     user = User.get_or_create(match.group(1))
     login_user(user)
-    return redirect(url_for('hidden'))
+    return redirect(url_for('index'))
 
 
 @app.route('/logout')
